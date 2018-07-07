886c08f5bad7c25f0d6535ad25ca2e787728b7c3
Ian Harwell <sirharwell@gmail.com>
2018-07-07 17:00:17 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources :products
   end
 
-  #Do not place any routes below this one
   get '*other', to: 'static#index'
 end
