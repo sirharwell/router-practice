a2a4f852cf91bd79ad6fef5d379491e7b28302bb
Ian Harwell <sirharwell@gmail.com>
2018-07-09 23:59:58 -0600

    app

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import NoMatch from './components/NoMatch';
 
 const App = () => (
   <div>
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
+    <Route component={NoMatch} />
   </div>
 );
-
-export default App;
