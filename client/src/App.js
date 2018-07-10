4397586d0269724b0b0be968542608b12fb10c36
Ian Harwell <sirharwell@gmail.com>
2018-07-10 00:02:33 -0600

    nomatch

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import NoMatch from './components/NoMatch';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Route exact path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route component={NoMatch} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
   </div>
 );
+
+export default App;
