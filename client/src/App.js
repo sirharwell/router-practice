92c12c1c77d4fa9611cb107353272619d694b496
Ian Harwell <sirharwell@gmail.com>
2018-07-14 23:57:43 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import Home from './components/Home';
 import About from './components/About';
 import NoMatch from './components/NoMatch';
 import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/Navbar';
 
 const App = () => (
   <div>
+    <NavBar />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
