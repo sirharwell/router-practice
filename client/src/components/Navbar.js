91ca08cb73ea7679c437ce1645e6f2cb6b3c40df
Ian Harwell <sirharwell@gmail.com>
2018-07-16 11:53:15 -0600

    fix navi bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { isAuthenticated } from '../fakeAuth';
 
 const styles = {
   active: {
-    textDecoration: 'none',
+    textDecoration: 'underline',
     fontWeight: 'bold',
     color: 'black',
   }
@@ -14,6 +15,11 @@ const NavBar = () => (
     <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
     {' '}
     <NavLink activeStyle={styles.active} to="/about">About</NavLink>
+    { isAuthenticated() ?
+        <NavLink activeStyle={styles.active} to="/dashboard">Dashboard</NavLink>
+        :
+        <NavLink activeStyle={styles.active} to="/login">Login</NavLink>
+    }
   </nav>
 )
 
