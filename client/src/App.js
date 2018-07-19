b6c69a1b88578b042e82cca1ee84215f7088ffe4
Ian Harwell <sirharwell@gmail.com>
2018-07-18 23:58:49 -0600

    protected

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import NavBar from './components/Navbar';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Product from './components/Product';
+import ProtectedRoute from './components/ProtectedRoute';
 
 
 const App = () => (
@@ -16,6 +17,8 @@ const App = () => (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/products/:id" component={Product} />
+      <ProtectedRoute path="/dashboard" component={Dashboard} />
+      <ProtectedRoute path="/products/:id" component={Product} />
       <Route path="/dashboard" component={Dashboard} />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
